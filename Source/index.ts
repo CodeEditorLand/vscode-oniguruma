a74a66c302506b4dd2ad958d4c8ab29f974f8e7b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-13 18:29:36 +0300

    squash!

--- Source/index.ts
+++ Source/index.ts
@@ -293,7 +293,6 @@ export class OnigString implements IOnigString {
 		if (!onigBinding) {
 			throw new Error(`Must invoke loadWASM first.`);
 		}
-
 		this._onigBinding = onigBinding;
 		this.content = str;
 		const utfString = new UtfString(str);
